1baea412f5bce9ec8235a69385535e5374624875
jacob.gahn@gmail.com <jacob.gahn@gmail.com>
2025-01-22 14:18:39 -0500

    added reference index endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req: Request, res: Response, next: NextFunction) {
-	res.send("Hello World\n");
+	res.send("Hello Trunk Tools!\nAvailable routes: /convert\n");
 });
 
 module.exports = router;
